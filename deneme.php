0da200389ab89b1839ff037954910000ff0108d5
iremcebecii <iremcebeci3437@gmail.com>
2024-10-11 11:29:51 +0300

    Add files via upload
    internet programcılığı 2.hafta ders notları 11.10.2024

---
+++ deneme.php
@@ -0,0 +1,73 @@
+<?php
+/*$degisken=true;
+$degisken2=3;
+$degisken3=5.6;
+$degisken4="irem";
+$degisken5=array("red","blue","yellow");
+$degisken6=new stdClass;
+$degisken7=NULL;
+$degisken8="30";
+
+echo gettype($degisken)."<br>".
+gettype($degisken2)."<br>".
+gettype($degisken3)."<br>".
+gettype($degisken4)."<br>".
+gettype($degisken5)."<br>".
+gettype($degisken6)."<br>".
+gettype($degisken7)."<br>".
+gettype($degisken8)."<br>";
+
+$sayi=55;
+echo "<b>birinci degiskenin adı:
+\$sayi</b>"."<br>".
+    "değeri:".$sayi."<br>".
+    "türü:".gettype($sayi)."<br>";
+
+$adi="irem";
+echo "<b>ikinci degiskenin adı:
+\$adi</b>"."<br>". 
+    "değeri:".$adi."<br>". 
+    "türü:".gettype($adi)."<br>";
+
+$ondalik=5.1234;
+echo "<b>üçüncü degiskenin adı:
+\$ondalik</b>"."<br>". 
+    "değeri:".$ondalik."<br>". 
+    "türü:".gettype($ondalik)."<br>";
+
+
+$mantiksal=1;
+echo "<b>dördüncü degiskenin adı:
+\$mantiksal</b>"."<br>". 
+    "değeri:".$mantiksal."<br>". 
+    "türü:".gettype($mantiksal)."<br>";
+
+
+$a="32";
+settype($a,"integer");
+var_dump($a);
+echo gettype($a)."<br>";
+
+$b=32;
+settype($b,"string");
+var_dump($b);
+echo gettype($b)."<br>";
+
+$c=true;
+settype($c,"integer");
+var_dump($c);
+echo gettype($c)."<br>";*/
+
+//php sabitleri
+echo "<h2>php sabitleri</h2>"."<br>".
+     "kodun bulunduğu yer:".(__LINE__)."<br>". 
+     "dosyanın bulunduğu yer:".(__FILE__)."
+     <br>". 
+     "sunucudaki PHP versiyonu:
+     ".phpversion()."<br>". 
+     "sunucudaki işletim sistemi:".(PHP_OS)."
+     <br>".
+    "dosyanın bulunduğu dizin:".(__DIR__);
+    ?>
+
+
